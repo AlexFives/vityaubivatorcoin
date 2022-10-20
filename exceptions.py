c9f953fd7bab4d8ac0dd893a95bdb6e56abaa2bc
AlexFives <alex.emelyanov.rg03@gmail.com>
2022-10-20 06:01:54 +0300

    v0

--- exceptions.py
+++ exceptions.py
@@ -2,3 +2,9 @@ class NotEnoughCoins(Exception):
     def __init__(self, user: str):
         message = f"User {user} has not enough coins!"
         super().__init__(message)
+
+
+class TooLittleAmount(Exception):
+    def __init__(self, amount: str):
+        message = f"Amount {amount} is too small!"
+        super().__init__(message)
--- transactions_database.py
+++ transactions_database.py
@@ -1,41 +1,66 @@
 from decimal import Decimal
 from typing import Iterator
+import threading
+import time
 
 from transaction import Transaction
-from exceptions import NotEnoughCoins
+from exceptions import NotEnoughCoins, TooLittleAmount
 
 
 class TransactionsDatabase:
-    def __init__(self):
+
+    def __init__(self,
+                 host_name: str,
+                 root_user: str = 'root',
+                 minimal_transaction_amount: Decimal = Decimal(1e-3)):
         self.__transactions = list()
+        self.__run_miner_thread(host_name)
+        self.__root_user = root_user
+        self.__minimal_transaction_amount = minimal_transaction_amount
 
-    def make_transaction(self, sender_pkh: str, receiver: str, amount: Decimal):
-        sender_balance = self.get_balance(sender_pkh)
+    def __run_miner_thread(self, host_name: str):
+        miner_thread = threading.Thread(target=self.__miner_thread, args=(host_name,), daemon=True)
+        miner_thread.start()
+
+    def __miner_thread(self, host_name: str):
+        previous_reward_time = 0.0
+        while True:
+
+
+
+    def make_transaction(self, sender_name: str, receiver_name: str, amount: Decimal):
+        if amount <
+
+        if sender_name == self.__root_user:
+            # чтобы исключить возможность транзаций от пользователя с бесконечным балансом
+            raise NotEnoughCoins(sender_name)
+
+        sender_balance = self.get_balance(sender_name)
         if sender_balance < amount:
-            raise NotEnoughCoins(sender_pkh)
-        self.__apply_transaction(sender_pkh, receiver, amount)
+            raise NotEnoughCoins(sender_name)
+        self.__apply_transaction(sender_name, receiver_name, amount)
 
-    def get_balance(self, pkh: str) -> Decimal:
-        taken = self.__count_taken_coins(pkh)
-        given = self.__count_given_coins(pkh)
+    def get_balance(self, name: str) -> Decimal:
+        taken = self.__count_taken_coins(name)
+        given = self.__count_given_coins(name)
         return taken - given
 
-    def __count_taken_coins(self, sender_pkh: str) -> Decimal:
+    def __count_taken_coins(self, sender_name: str) -> Decimal:
         taken = Decimal(0)
         for transaction in self.__transactions:
-            if transaction.receiver == sender_pkh:
+            if transaction.receiver == sender_name:
                 taken += transaction.amount
         return taken
 
-    def __count_given_coins(self, sender_pkh: str) -> Decimal:
+    def __count_given_coins(self, sender_name: str) -> Decimal:
         given = Decimal(0)
         for transaction in self.__transactions:
-            if transaction.sender == sender_pkh:
+            if transaction.sender == sender_name:
                 given += transaction.amount
         return given
 
-    def __apply_transaction(self, sender_pkh: str, receiver_pkh: str, amount: Decimal):
-        transaction = Transaction(sender_pkh, receiver_pkh, amount)
+    def __apply_transaction(self, sender_name: str, receiver_name: str, amount: Decimal):
+        transaction = Transaction(sender_name, receiver_name, amount)
         self.__transactions.append(transaction)
 
     @property
